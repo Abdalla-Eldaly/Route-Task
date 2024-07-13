19973acc9bf41bb3a331e09c691706e156c8feca
abdalla eldaly <abdalla90eldaly90@gmail.com>
2024-07-13 10:37:58 +0300

    Add API Service Client (Retrofit).

---
+++ lib/data/network/app_api.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
+
+import '../../app/constants.dart';
+import '../response/response.dart';
+part 'app_api.g.dart';
+
+@RestApi(baseUrl: Constants.baseUrl)
+abstract class AppServicesClient {
+  factory AppServicesClient(Dio dio, {String baseUrl}) = _AppServicesClient;
+
+  @GET('/products')
+  Future<ProductListResponse> getHomeProductData();
+}
+
+class AppServiceClientImpl {
+  final AppServicesClient _client;
+
+  AppServiceClientImpl(Dio dio)
+      : _client = AppServicesClient(dio);
+
+  Future<ProductListResponse> getHomeProductData() {
+    return _client.getHomeProductData();
+  }
+}
