fda9783ab1c14b207bc3704bfb2e931326150c36
abdalla eldaly <abdalla90eldaly90@gmail.com>
2024-07-13 10:35:13 +0300

    Creating Mappers and Applying Null Safety.

---
+++ lib/app/constants.dart
@@ -0,0 +1,15 @@
+class Constants {
+  Constants._();
+
+  static const String baseUrl = "https://dummyjson.com";
+  static const int apiTimeOut = 10000;
+  static const String empty = "";
+  static const int zero = 0;
+  static const double zeroForD = 0.0;
+  static const bool falseBool = false;
+
+  static const double zeroDouble = 0.0;
+  static const List<String> emptyList = [];
+  static const String defaultDateTime = '1970-01-01T00:00:00Z';
+
+}
