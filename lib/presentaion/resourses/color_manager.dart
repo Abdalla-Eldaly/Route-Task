2252488d0e019093def73dc8e6e900c64aa73c7a
abdalla eldaly <abdalla90eldaly90@gmail.com>
2024-07-13 10:17:18 +0300

    Create Colors Manger

---
+++ lib/presentaion/resourses/color_manager.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  ColorManager._();
+
+  static const Color primary = Color(0xff004182);
+  static const Color secondary = Color(0xFF0776BB);
+
+  static const Color black = Color(0xff101719);
+  static const Color white = Color(0xFFF3F6F7);
+  static const Color error = Color(0xFFD70B0B);
+
+  static const Color transparent = Colors.transparent;
+}
